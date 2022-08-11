4bf59295a093ced600d9021a4f0452f4a579231e
tasim313 <mostasimmahmudtasim@gmail.com>
2022-08-11 14:55:00 +0600

    modify admin panel create adminModel

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import *
 
-# Register your models here.
+
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'gender', 'date_of_birth', 'joining_date', 'is_active')
+    list_filter = ('email',)
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'gender', 'date_of_birth', 'joining_date', 'is_active')
+    date_hierarchy = 'joining_date'
+    ordering = ('id', )
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+
+
+
+################### Changing Admin Header ########################################
+
+admin.site.site_header = "Track Enterprise Customers"
+admin.site.index_title = " Track Enterprise Customers Administration Panel"
+admin.site.site_title = "Track Enterprise Customers"
+
